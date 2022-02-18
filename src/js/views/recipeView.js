ebcd3437d36d0566a77ba03d71cd54260969d3c2
Shaun <n10537848@qut.edu.au>
2022-02-18 16:48:01 +1000

    fix the fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,6 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import { fracty } from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,7 +128,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? fracty(ing.quantity).toString() : ''
+          ing.quantity ? ing.quantity.toString() : ''
         }</div>
         <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
